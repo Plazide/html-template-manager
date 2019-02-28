36c0d864b6684d087273d157a7d9cb4a46832c5d
20641118 <callehj@hotmail.se>
2019-03-01 00:06:21 +0100

    Improve error handling

--- bin/template.js
+++ bin/template.js
@@ -4,7 +4,4 @@ const args = process.argv;
 const template = args[2];
 const static = args[3];
 
-if(!template || !static)
-	throw new Error("Missing arguments");
-
 templates.listen(template, static);
