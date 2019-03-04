dbab82ae21de3ef55ce2a06431ed0918f5ceb277
20641118 <callehj@hotmail.se>
2019-03-04 18:46:35 +0100

    Change cli argument behaviour

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
-const templates = require("../lib/index");
+const fs = require("fs");
+const script = require("../lib/index");
+const config = require("../data/config.json");
 const args = process.argv;
-const template = args[2];
-const static = args[3];
+let template = args[2];
+let static = args[3];
 
-templates.listen(template, static);
+if(template || static){
+	// If template or static arguments are passed to the cli,
+	// set the config values to the passed values and save the new config.
+	config.static_folder = static;
+	config.template_file = template;
+
+	fs.writeFileSync(JSON.stringify(config), "../data/config.json");
+}else{
+	static = config.static_folder;
+	template = config.template_file;
+}
+
+script.listen(template, static);
